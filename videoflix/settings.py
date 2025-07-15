93e9219ac1fe6c22821213ec4d6d11f6105890eb
Anja Gollner <anjakgollner@gmail.com>
2025-07-15 11:48:57 +0200

    allowed hosts

--- videoflix/settings.py
+++ videoflix/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = ['localhost', '127.0.0.1','anja-gollner.com', 'www.anja-gollner.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1','anja-gollner.com', 'www.anja-gollner.com', 'videoflix-backend-3zyr.onrender.com']
 
 # Application definition
 EMAIL_BACKEND = 'videoflix.custom_email_backend.CustomEmailBackend'
